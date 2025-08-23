bc7d1a252ead4df418287c892fd45cbbd281c2d3
Keeran Parthipan <keeran.parthipan@gmail.com>
2025-08-23 13:15:31 +0100

    Preprocess dataset

--- model.py
+++ model.py
@@ -1,1 +1,17 @@
 # Do some preprocessing then train simple model
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Load dataset
+df = pd.read_csv('creditcard.csv')
+
+# Remove duplicate rows
+df = df.drop_duplicates()
+
+# Split into features and target
+X = df.drop('Class', axis=1)
+y = df['Class']
+
+# Split the data. Will use 20% for testing, 80% for training + validation
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, stratify=y)
