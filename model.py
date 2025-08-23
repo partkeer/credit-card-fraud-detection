70c973bc9f28391c0f718b3b4990e6645ed3c0ab
Keeran Parthipan <keeran.parthipan@gmail.com>
2025-08-23 16:07:04 +0100

    Train initial model
    Train random forest, and evaluate using different metrics.

--- model.py
+++ model.py
@@ -1,7 +1,10 @@
 # Do some preprocessing then train simple model
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, precision_recall_curve, auc, roc_curve, roc_auc_score
 
 # Load dataset
 df = pd.read_csv('creditcard.csv')
@@ -15,3 +18,32 @@ y = df['Class']
 
 # Split the data. Will use 20% for testing, 80% for training + validation
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, stratify=y)
+
+# Train random forest model
+model = RandomForestClassifier(n_estimators=100, random_state=1)
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+# Probability of positive class
+y_score = model.predict_proba(X_test)[:,1]
+
+# Evaluate predictions
+confusion_mat = confusion_matrix(y_test, y_pred)
+print('Confusion matrix:\n',confusion_mat)
+
+precision, recall, thresholds = precision_recall_curve(y_test, y_score)
+plt.plot(recall, precision)
+plt.title('Precision recall curve')
+plt.show()
+
+auc_pr = auc(recall, precision)
+print('Area under precision-recall curve:', auc_pr)
+
+fpr, tpr, thresholds_roc = roc_curve(y_test, y_score)
+plt.plot(fpr, tpr)
+plt.title('ROC curve')
+plt.show()
+
+auc_roc = roc_auc_score(y_test, y_score)
+print('Area under ROC curve:', auc_roc)
