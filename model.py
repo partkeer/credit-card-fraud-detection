464c87d290ee381f091ccec7f27442a3190e552d
Keeran Parthipan <keeran.parthipan@gmail.com>
2025-08-28 16:07:09 +0100

    Combine XGBoost and KNN predictions

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, precision_recall_curve, auc, roc_curve, roc_auc_score
 from xgboost import XGBClassifier
 
@@ -33,18 +34,30 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # Create a pipeline which scales data, then applies model
 # Use xgboost
-pipe = Pipeline([
+xgb_pipe = Pipeline([
     ('scaler', StandardScaler()),
     # hyperparameters are tuned
     ('model', XGBClassifier(n_estimators=300, learning_rate=0.05, min_child_weight=2, subsample=0.9, colsample_bytree=0.5))
 ])
 
-pipe.fit(X_train, y_train)
+# Use knn
+knn_pipe = Pipeline([
+    ('scaler', StandardScaler()),
+    ('model', KNeighborsClassifier(n_neighbors=3))
+])
 
-# Make predictions
-y_pred = pipe.predict(X_test)
+xgb_pipe.fit(X_train, y_train)
+knn_pipe.fit(X_train, y_train)
+
 # Probability of positive class
-y_score = pipe.predict_proba(X_test)[:,1]
+y_score_xgb = xgb_pipe.predict_proba(X_test)[:,1]
+y_score_knn = knn_pipe.predict_proba(X_test)[:,1]
+
+# Use simple averaging for final prediction
+# (xgboost tends to be too cautious and knn tends to be under-cautious when predicting positive classes;
+# using a simple average consistently increases f1-score across test sets)
+y_score = 0.5 * (y_score_xgb + y_score_knn)
+y_pred = np.round(y_score)
 
 # Evaluate predictions
 confusion_mat = confusion_matrix(y_test, y_pred)
