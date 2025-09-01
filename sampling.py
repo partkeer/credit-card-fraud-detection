8dc20bc53eb50e13e22526e2f0d731b3ef4c7c1b
Keeran Parthipan <keeran.parthipan@gmail.com>
2025-09-01 11:13:30 +0100

    Explore effects of undersampling
    Use NearMiss algorithm and train SVC

--- sampling.py
+++ sampling.py
@@ -1,4 +1,4 @@
-# Train logistic regression using original and oversampled data, and compare results.
+# Train logistic regression/SVC using oversampled/undersampled data, and compare results.
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -8,6 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, f1_score
 from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import NearMiss
 
 # Load dataset
 df = pd.read_csv('creditcard.csv')
@@ -44,7 +45,7 @@ smote = SMOTE(random_state=1, k_neighbors=5)
 X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
 print('\n----- After oversampling -----')
 print(f'Shape: {X_train_resampled.shape}')
-print(f'Number of positive samples: {sum(y_train_resampled== 1)}')
+print(f'Number of positive samples: {sum(y_train_resampled == 1)}')
 print(f'Number of negative samples: {sum(y_train_resampled == 0)}')
 
 # train a logistic regression model with oversampled dataset
@@ -56,3 +57,23 @@ model2.fit(X_train_resampled, y_train_resampled)
 y_pred2 = model2.predict(X_test)    # model predicts more positives as expected (leading to worse performance)
 print(f'Confusion matrix: \n{confusion_matrix(y_test, y_pred2)}')
 print('F1 score: ', f1_score(y_test, y_pred2))
+
+
+
+# create undersampled dataset
+nm = NearMiss(sampling_strategy=0.3, version=1, n_neighbors=3)
+X_train_undersampled, y_train_undersampled = nm.fit_resample(X_train, y_train)
+
+print('\n----- After undersampling (SVM) -----')
+print(f'Shape: {X_train_undersampled.shape}')
+print(f'Number of positive samples: {sum(y_train_undersampled== 1)}')
+print(f'Number of negative samples: {sum(y_train_undersampled == 0)}')
+
+model3 = Pipeline([
+    ('scaler', StandardScaler()),
+    ('model', SVC())
+])
+model3.fit(X_train_undersampled, y_train_undersampled)
+y_pred3 = model3.predict(X_test)
+print(f'Confusion matrix: \n{confusion_matrix(y_test, y_pred3)}')
+print('F1 score: ', f1_score(y_test, y_pred3))
